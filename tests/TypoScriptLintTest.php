c6fdb7553b2b670ad5525bb53053a5ec12e292e0
Sebastian Hofer <sebastian.hofer@marit.ag>
2019-11-26 12:35:47 +0000

    :white_check_mark: Add additional tests

---
+++ tests/TypoScriptLintTest.php
@@ -0,0 +1,112 @@
+<?php
+declare(strict_types=1);
+
+namespace Pluswerk\TypoScriptLinter\Tests;
+
+use GrumPHP\Collection\FilesCollection;
+use GrumPHP\Configuration\GrumPHP;
+use GrumPHP\Runner\TaskResult;
+use GrumPHP\Task\Context\RunContext;
+use PHPUnit\Framework\TestCase;
+use Pluswerk\TypoScriptLinter\TypoScriptLint;
+use Pluswerk\TypoScriptLinter\TypoScriptLinter;
+
+/**
+ * Class TypoScriptLintTest
+ * @package Pluswerk\TypoScriptLinter\Tests
+ * @covers \Pluswerk\TypoScriptLinter\TypoScriptLint
+ * @covers \Pluswerk\TypoScriptLinter\TypoScriptLinter
+ * @covers \Pluswerk\TypoScriptLinter\TypoScriptLintError
+ */
+final class TypoScriptLintTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider failingTestFilesProvider
+     */
+    public function ifTypoScriptLinterDetectsAFailureTheTaskFails($filePath): void
+    {
+        $grumphp = $this->createMock(GrumPHP::class);
+        $typoScriptLinter = new TypoScriptLinter();
+
+        $file = new \SplFileInfo($filePath);
+        $fileCollection = new FilesCollection();
+        $fileCollection->add($file);
+        $context = new RunContext($fileCollection);
+
+        $typoScriptLint = new TypoScriptLint($grumphp, $typoScriptLinter);
+
+        $result = $typoScriptLint->run($context);
+
+        $this->assertSame(TaskResult::FAILED, $result->getResultCode());
+    }
+
+    public function failingTestFilesProvider(): array
+    {
+        $basePath = __DIR__ . '/Fixtures/';
+        return [
+            'nesting' => [
+                'filePath' => $basePath . 'nesting.typoscript'
+            ],
+            'whitespace 01' => [
+                'filePath' => $basePath . 'whitespace_01.typoscript'
+            ],
+            'whitespace 02' => [
+                'filePath' => $basePath . 'whitespace_02.typoscript'
+            ],
+            'dead code' => [
+                'filePath' => $basePath . 'deadCode.typoscript'
+            ],
+            'indentation' => [
+                'filePath' => $basePath . 'indentation.typoscript'
+            ],
+            'repeating values' => [
+                'filePath' => $basePath . 'repeatingValues.typoscript'
+            ],
+            'duplicate assignment' => [
+                'filePath' => $basePath . 'duplicateAssignment.typoscript'
+            ],
+            'empty blocks' => [
+                'filePath' => $basePath . 'emptyBlocks.typoscript'
+            ]
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function ifNoFilesAreGivenTheTaskIsSkipped(): void
+    {
+        $grumphp = $this->createMock(GrumPHP::class);
+        $typoScriptLinter = new TypoScriptLinter();
+
+        $fileCollection = new FilesCollection();
+        $context = new RunContext($fileCollection);
+
+        $typoScriptLint = new TypoScriptLint($grumphp, $typoScriptLinter);
+
+        $result = $typoScriptLint->run($context);
+
+        $this->assertSame(TaskResult::SKIPPED, $result->getResultCode());
+    }
+
+    /**
+     * @test
+     */
+    public function ifNoLintingFailuresDetectedTheTaskPasses(): void
+    {
+        $grumphp = $this->createMock(GrumPHP::class);
+        $typoScriptLinter = new TypoScriptLinter();
+
+        $file = new \SplFileInfo(__DIR__ . '/Fixtures/passing.typoscript');
+        $fileCollection = new FilesCollection();
+        $fileCollection->add($file);
+        $context = new RunContext($fileCollection);
+
+        $typoScriptLint = new TypoScriptLint($grumphp, $typoScriptLinter);
+
+        $result = $typoScriptLint->run($context);
+
+        $this->assertSame(TaskResult::PASSED, $result->getResultCode());
+    }
+}
